ceaf4ae7a92df419837cf1b07d0dd9381dc8f162
jaiyesh <jaiyesh0002@gmail.com>
2024-09-29 12:30:46 +0530

    circle feature added

--- reservoir_applications.py
+++ reservoir_applications.py
@@ -8,7 +8,7 @@ st.sidebar.title("User Inputs")
 
 
 application = st.sidebar.selectbox("Select the Reservoir Engineering Application: ",
-                                   ("Pressure Profile", "Klinkenberg Effect"),)
+                                   ("Pressure Profile", "Klinkenberg Effect","circle"),)
 
 
 if application == "Pressure Profile":
@@ -118,6 +118,48 @@ elif application == "Klinkenberg Effect":
 
         st.pyplot(fig)
 
+
+elif application == "circle":
+    st.subheader("circle")
+
+    r = st.sidebar.slider("Enter the radius of circle", min_value = 0, max_value = 20, value=10 )
+
+
+    x = np.linspace(-r,r,1000)
+        
+    y_p = (r**2 - x**2)**0.5
+        
+    y_n = -y_p
+
+
+
+    b = st.sidebar.button(f"Create Circle of radius {r}")
+
+
+    if b: 
+        st.write(f"Circle Craetion of radius {r}")
+
+        plt.style.use("classic")
+
+        plt.figure(figsize=(6,12))
+
+        fig,ax = plt.subplots()
+
+        ax.plot(x,y_p, linewidth =4)
+        ax.plot(x,y_n, linewidth = 4)
+
+        ax.grid(True)
+
+        ax.set_title("CIRCLE")
+
+        ax.set_xlabel("X axis")
+        ax.set_ylabel("Y axis")
+
+        ax.set_xlim(-20,20)
+
+        ax.set_ylim(-20,20)
+
+        st.pyplot(fig)
 
 
 
